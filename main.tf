4e798e84ea9bfccf1ae6bbf379e13496f955e037
flakkv <aymankorban@gmail.com>
2023-10-21 03:53:02 +0300

    changed to docker compose

--- main.tf
+++ main.tf
@@ -3,32 +3,47 @@ provider "aws" {
 }
 
 resource "aws_instance" "ghost_server" {
-  ami           = "ami-06dd92ecc74fdfb36"  # Remember to replace this with the appropriate AMI for your region.
+  ami           = "ami-06dd92ecc74fdfb36"  # Replace this with the appropriate AMI for your region.
   instance_type = "t2.micro"
   key_name      = "e570"  # Replace with the name of your existing key.
   vpc_security_group_ids = [aws_security_group.ghost_sg.id]
 
   user_data = <<-EOT
               #!/bin/bash
-              adduser ghostuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-              echo "ghostuser:ghost_password" | chpasswd
-              usermod -aG sudo ghostuser
-
               apt-get update
-              apt-get upgrade -y
-              apt-get install -y nginx mysql-server ca-certificates curl gnupg ufw
-
-              ufw allow 'Nginx Full'
-
-              mkdir -p /etc/apt/keyrings
-              curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-              echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x $(lsb_release -cs) main" > /etc/apt/sources.list.d/nodesource.list
-
+              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
               apt-get update
-              apt-get install -y nodejs
-              npm install ghost-cli@latest -g
-
-              su - ghostuser -c 'mkdir -p /var/www/ghost && cd /var/www/ghost && ghost install --db=mysql --dbhost=localhost --dbuser=root --dbname=ghost_db --url=http://temporary-url-placeholder --no-prompt --no-stack'
+              apt-get install -y docker-ce
+              curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose
+              
+              # Create Docker Compose file
+              cat > /root/docker-compose.yml <<EOF
+version: '3.1'
+services:
+  ghost:
+    image: ghost:4-alpine
+    restart: always
+    ports:
+      - 8080:2368
+    environment:
+      database__client: mysql
+      database__connection__host: db
+      database__connection__user: root
+      database__connection__password: example
+      database__connection__database: ghost
+      url: http://localhost:8080
+  db:
+    image: mysql:8.0
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: example
+EOF
+              
+              # Start Docker Compose
+              docker-compose -f /root/docker-compose.yml up -d
   EOT
 
   tags = {
@@ -41,8 +56,8 @@ resource "aws_security_group" "ghost_sg" {
   description = "Security group for Ghost Server"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
