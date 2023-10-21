c0d64e3ab91ba0b437e99f86a41967c0a66b5930
flakkv <aymankorban@gmail.com>
2023-10-21 09:50:36 +0300

    docker fix

--- main.tf
+++ main.tf
@@ -3,26 +3,32 @@ provider "aws" {
 }
 
 resource "aws_instance" "ghost_server" {
-  ami           = "ami-06dd92ecc74fdfb36"  # Replace this with the appropriate AMI for your region.
+  ami           = "ami-06dd92ecc74fdfb36"  # Remember to replace this with the appropriate AMI for your region.
   instance_type = "t2.micro"
   key_name      = "e570"  # Replace with the name of your existing key.
   vpc_security_group_ids = [aws_security_group.ghost_sg.id]
 
   user_data = <<-EOT
               #!/bin/bash
+              # Add Docker's official GPG key:
               apt-get update
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              apt-get install -y ca-certificates curl gnupg
+              install -m 0755 -d /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              chmod a+r /etc/apt/keyrings/docker.gpg
+
+              # Add the repository to Apt sources:
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
               apt-get update
-              apt-get install -y docker-ce
-              curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              chmod +x /usr/local/bin/docker-compose
-              
+
+              apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
               # Create Docker Compose file
               cat > /root/docker-compose.yml <<EOF
 version: '3.1'
+
 services:
+
   ghost:
     image: ghost:4-alpine
     restart: always
@@ -35,14 +41,15 @@ services:
       database__connection__password: example
       database__connection__database: ghost
       url: http://localhost:8080
+
   db:
     image: mysql:8.0
     restart: always
     environment:
       MYSQL_ROOT_PASSWORD: example
 EOF
-              
-              # Start Docker Compose
+
+              # Start services with Docker Compose
               docker-compose -f /root/docker-compose.yml up -d
   EOT
 
@@ -56,8 +63,8 @@ resource "aws_security_group" "ghost_sg" {
   description = "Security group for Ghost Server"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
